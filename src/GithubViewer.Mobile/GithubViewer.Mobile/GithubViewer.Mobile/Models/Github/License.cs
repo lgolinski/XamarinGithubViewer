486cb003145aec7e9b35d59bb20e2b2ecec26c8d
Łukasz <lukasz.golinski@mail.com>
2020-05-21 16:45:30 +0200

    Added Github repository Domain models. https://jsonutils.com/

---
+++ src/GithubViewer.Mobile/GithubViewer.Mobile/GithubViewer.Mobile/Models/Github/License.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GithubViewer.Core.Models.Github
+{
+    public class License
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string SpdxId { get; set; }
+        public string Url { get; set; }
+        public string NodeId { get; set; }
+    }
+}
---
+++ src/GithubViewer.Mobile/GithubViewer.Mobile/GithubViewer.Mobile/Models/Github/RepositoryResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GithubViewer.Core.Models.Github
+{
+    public class RepositoryResponse
+    {
+        public int TotalCount { get; set; }
+        public bool IncompleteResults { get; set; }
+        public IList<Repository> Items { get; set; }
+    }
+}
